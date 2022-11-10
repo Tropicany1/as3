3c1d1e6a4dea80bb9468fb80aaeab3ebe463d6ec
Tiffany <wutiffany0331@gmail.com>
2022-11-09 20:03:53 -0500

    Made the get button work

--- script.js
+++ script.js
@@ -7,21 +7,28 @@ const getData = async (url, params) => {
   };
   
   const getMovies1 = async () => {
-    const movieData = await getData("https://api.themoviedb.org/3/search/movie", {
-      params: {
-        api_key: "541f480956a206bb8529fb019433027e",
-        query: "despicable me",
+    let movieInfo= document.getElementById("movieInfo");
+    movieInfo.innerHTML="";
+    let selectedMovie = document.getElementById("movieselect")
+    const movieData = await getData ("https://api.themoviedb.org/3/search/movie", {
+    params: {
+      api_key: "541f480956a206bb8529fb019433027e",
+        query: selectedMovie.value,
       }
     });
-  
-    for (let movie of movieData.data.results) {
+
+    if (movieData.data.results.length < 1) {
+      return;
+    }
+    
+    let movie= movieData.data.results[0];
       const extraData = await getData(`https://api.themoviedb.org/3/movie/${movie.id}`, {
         params: {
           api_key: "541f480956a206bb8529fb019433027e",
           append_to_response: "videos",
         }
       });
-  
+ 
       const trailer = extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key;
       const p = document.createElement('p');
       const img = document.createElement('img');
@@ -31,42 +38,12 @@ const getData = async (url, params) => {
       img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
       iframe.src = `https://www.youtube.com/embed/${trailer}`
   
-      document.body.append(p);
-      document.body.append(img);
-      document.body.append(iframe);
+      movieInfo.append(p);
+      movieInfo.append(img);
+      movieInfo.append(iframe);
     }
-  };
-  
-  const getMovies2 = async () => {
-    const movieData = await getData("https://api.themoviedb.org/3/search/movie", {
-      params: {
-        api_key: "541f480956a206bb8529fb019433027e",
-        query: "despicable me",
-      }
-    });
-  
-    movieData.data.results.forEach(async (movie) => {
-      const extraData = await getData(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-        params: {
-          api_key: "541f480956a206bb8529fb019433027e",
-          append_to_response: "videos",
-        }
-      }); 
-  
-      const trailer = extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key;
-      const p = document.createElement('p');
-      const img = document.createElement('img');
-      const iframe = document.createElement('iframe');
-  
-      p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-      iframe.src = `https://www.youtube.com/embed/${trailer}`
-  
-      document.body.append(p);
-      document.body.append(img);
-      document.body.append(iframe);
-    });
-  };
+
+  ;
   
   getMovies1();
-  getMovies2();
+  
